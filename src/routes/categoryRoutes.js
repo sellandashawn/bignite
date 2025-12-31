9cc1958a6ede168557eca39a316a9c87391534ef
DJshenal <pererashenal2001@gmail.com>
2025-12-31 09:21:03 +0530

    Feat - update category

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const {
   addCategory,
   getCategories,
+  getCategoriesByType,
 } = require("../controllers/categoryController");
 const authenticateUser = require("../middleware/authMiddleware");
 
 router.post("/", authenticateUser, addCategory);
 
 router.get("/", getCategories);
+
+router.get("/type/:type", getCategoriesByType);
 
 module.exports = router;
